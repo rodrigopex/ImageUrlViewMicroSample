ecd816194beb0af8dacf2c8ce32c4599537eb065
Rodrigo Peixoto <rodrigopex@gmail.com>
2014-04-04 18:15:24 -0300

    Redirect URL implemented

--- src/ImageUrlLoader.cpp
+++ src/ImageUrlLoader.cpp
@@ -46,24 +46,53 @@ void ImageUrlLoader::loadImageUrl() {
 
 }
 
-void ImageUrlLoader::replyFinished(QNetworkReply * reply) {
-	disconnect(m_manager, SIGNAL(finished(QNetworkReply*)), this,
-			SLOT(replyFinished(QNetworkReply*)));
-
-	QStringList splitList = m_imageUrl.split("/");
-	QString fileName(splitList.at(splitList.size() - 1));
-	m_file = new QFile("tmp/" + fileName);
-	if (!m_file->open(QIODevice::WriteOnly | QIODevice::Append)) {
-		qDebug() << "failed to open file";
-		return;
+QUrl redirectUrl(const QUrl& possibleRedirectUrl, const QUrl& oldRedirectUrl) {
+	QUrl redirectUrl;
+	/*
+	 * Check if the URL is empty and
+	 * that we aren't being fooled into a infinite redirect loop.
+	 * We could also keep track of how many redirects we have been to
+	 * and set a limit to it, but we'll leave that to you.
+	 */
+	if (!possibleRedirectUrl.isEmpty()
+			&& possibleRedirectUrl != oldRedirectUrl) {
+		redirectUrl = possibleRedirectUrl;
 	}
-	m_file->write(reply->readAll());
-	m_file->flush();
-	m_file->close();
+	return redirectUrl;
+}
 
-	//Its parent MUST have a imageSource property for being set
-	this->parent()->setProperty("imageSource", QVariant::fromValue(QUrl(m_imagePath)));
-	emit this->imageDone(m_imagePath);
-	delete m_manager;
-	m_manager = 0;
+void ImageUrlLoader::replyFinished(QNetworkReply * reply) {
+
+	QVariant possibleRedirectUrl = reply->attribute(
+			QNetworkRequest::RedirectionTargetAttribute);
+
+	/* We'll deduct if the redirection is valid in the redirectUrl function */
+	QUrl _urlRedirectedTo = redirectUrl(possibleRedirectUrl.toUrl(),
+			reply->request().url());
+
+	if (_urlRedirectedTo.isEmpty()) {
+
+		disconnect(m_manager, SIGNAL(finished(QNetworkReply*)), this,
+				SLOT(replyFinished(QNetworkReply*)));
+
+		QStringList splitList = m_imageUrl.split("/");
+		QString fileName(splitList.at(splitList.size() - 1));
+		m_file = new QFile("tmp/" + fileName);
+		if (!m_file->open(QIODevice::WriteOnly | QIODevice::Append)) {
+			qDebug() << "failed to open file";
+			return;
+		}
+		m_file->write(reply->readAll());
+		m_file->flush();
+		m_file->close();
+
+		//Its parent MUST have a imageSource property for being set
+		this->parent()->setProperty("imageSource",
+				QVariant::fromValue(QUrl(m_imagePath)));
+		emit this->imageDone(m_imagePath);
+		delete m_manager;
+		m_manager = 0;
+	} else {
+		m_manager->get(QNetworkRequest(_urlRedirectedTo));
+	}
 }
